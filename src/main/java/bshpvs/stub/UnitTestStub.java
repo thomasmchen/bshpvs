803c3164a8eacd4fb8c9d3bacb46fbab4fad4a71
Thomas <thomas99k@gmail.com>
2018-09-04 13:09:22 -0400

    core - add testing stubs for initial scaffolding of cicd

---
+++ src/main/java/bshpvs/stub/UnitTestStub.java
@@ -0,0 +1,16 @@
+package bshpvs.stub;
+
+/**
+ * Sample class to illustrate JUnit 5 Testing.
+ */
+public class UnitTestStub {
+
+    /**
+     * Sample function for unit testing example.
+     * @param message the input message
+     * @return sample string
+     */
+    public final String getMessage(final String message) {
+        return message;
+    }
+}
