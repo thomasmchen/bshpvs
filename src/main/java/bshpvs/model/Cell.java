56b2fc4d656b35b37d8db115fd745edc923e44c2
Thomas <thomas99k@gmail.com>
2018-09-07 22:23:09 -0400

    core - resolve class models for temporary coverage

--- src/main/java/bshpvs/model/Cell.java
+++ src/main/java/bshpvs/model/Cell.java
@@ -1,7 +1,2 @@
 package bshpvs.model;
 
-/**
- * Created by thomas on 9/7/18.
- */
-public class Cell {
-}
--- src/main/java/bshpvs/model/Game.java
+++ src/main/java/bshpvs/model/Game.java
@@ -1,7 +1,1 @@
 package bshpvs.model;
-
-/**
- * Created by thomas on 9/7/18.
- */
-public class Game {
-}
