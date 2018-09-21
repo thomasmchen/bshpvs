ea71eb7ffd9474221ecda1253b8a0c31271b5fce
Thomas <thomas99k@gmail.com>
2018-09-21 15:04:30 -0400

    core - add Cell and CellType classes with tests

--- src/main/java/bshpvs/model/CellType.java
+++ src/main/java/bshpvs/model/CellType.java
@@ -2,9 +2,27 @@ package bshpvs.model;
 
 
 /**
- * Created by thomas on 9/19/18.
+ * Enumeration to define Type of Cell
  */
 public enum CellType {
-    WATER,
-    SHIP
+    WATER(0),
+    SHIP(1);
+
+    private int value;
+
+    /**
+     * Retrieve integer value of cell type
+     * @return the integer corresponding to the cell type
+     */
+    public int getValue() {
+        return this.value;
+    }
+
+    /**
+     * Private constructor for enumeration class
+     * @param value the value of the Cell
+     */
+    private CellType(int value) {
+        this.value = value;
+    }
 }
---
+++ src/test/java/bshpvs/model/CellTest.java
@@ -0,0 +1,38 @@
+package bshpvs.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CellTest {
+    private static Cell c;
+
+    @BeforeEach
+    private void setup() {
+        c = new Cell();
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(CellType.WATER, c.getType());
+        assertEquals(false, c.isHit());
+    }
+
+    @Test
+    public void testHit() {
+        c.hit();
+        assertEquals(true, c.isHit());
+    }
+
+    @Test
+    public void testSetType() {
+        c.setType(CellType.SHIP);
+        assertEquals(CellType.SHIP, c.getType());
+    }
+
+    @Test void testToString() {
+        assertEquals("0", c.toString());
+    }
+}
