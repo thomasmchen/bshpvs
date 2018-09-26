9edd3ed77656b45fd96d7804e6b2fb9447e659fe
Dan Morton <dmorton2297@gmail.com>
2018-09-26 15:20:33 -0400

    Able to parse json from get ships obejct. Need to work on rendering the placed ships on the users view

--- client/battleship/src/app/game-window/game-window.component.ts
+++ client/battleship/src/app/game-window/game-window.component.ts
@@ -7,10 +7,10 @@
   "victoryMessage":"test",
   "ships":[
       {"identifier":0,"numSpaces":5,"spaces":[{"x":0,"y":0},{"x":1,"y":0},{"x":2,"y":0},{"x":3,"y":0},{"x":4,"y":0}]},
-      {"identifier":0,"numSpaces":5,"spaces":[{"x":0,"y":1},{"x":1,"y":1},{"x":2,"y":1},{"x":3,"y":1}]},
-      {"identifier":0,"numSpaces":5,"spaces":[{"x":0,"y":2},{"x":1,"y":2},{"x":2,"y":2}]},
-      {"identifier":0,"numSpaces":5,"spaces":[{"x":0,"y":3},{"x":1,"y":3},{"x":2,"y":3}]},
-      {"identifier":0,"numSpaces":5,"spaces":[{"x":0,"y":4},{"x":1,"y":4}]}
+      {"identifier":1,"numSpaces":5,"spaces":[{"x":0,"y":1},{"x":1,"y":1},{"x":2,"y":1},{"x":3,"y":1}]},
+      {"identifier":2,"numSpaces":5,"spaces":[{"x":0,"y":2},{"x":1,"y":2},{"x":2,"y":2}]},
+      {"identifier":2,"numSpaces":5,"spaces":[{"x":0,"y":3},{"x":1,"y":3},{"x":2,"y":3}]},
+      {"identifier":3,"numSpaces":5,"spaces":[{"x":0,"y":4},{"x":1,"y":4}]}
   ]
 }
 */
@@ -29,17 +29,37 @@ import { Http } from '@angular/http';
 export class GameWindowComponent implements OnInit {
 
 
+  public ships: Ship[] = new Array<Ship>();
+
+
   constructor(private http: Http) { 
     this.http.get('http://www.mocky.io/v2/5babd5cb310000550065455a').subscribe((res) => {
-      let result = res.json() as GameRequest;
-
-      this.renderShips(result);
+      let result = res.json() as GameResponse;
+      this.loadShips(result);
+      this.renderShips();
     });
   }
 
-  renderShips(result: GameRequest) {
+  loadShips(result: GameResponse) {
+    let _ships = result.ships;
     for (var i = 0; i < result.ships.length; i++) {
-      console.log(result.ships[i]);
+      var curr = _ships[i];
+      let id = curr.identifier;
+      let numSpaces = curr.numSpaces;
+      let spaces = curr.spaces;
+      var newShip : Ship =  {
+        "identifier": id,
+        "numSpaces": numSpaces,
+        "spaces": spaces
+      };
+
+      this.ships.push(newShip);
+    }
+  }
+
+  renderShips() {
+    for (var i = 0; i < this.ships.length; i++) {
+      console.log(this.ships[i]);
     }
   }
 
@@ -64,7 +84,7 @@ interface Ship {
   spaces: Coordinate[]
 }
 
-interface GameRequest {
+interface GameResponse {
   userId: number,
   userName: string,
   victoryMessage: string,
