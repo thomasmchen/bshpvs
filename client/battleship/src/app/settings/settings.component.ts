f0893325c684e29df6ce2723016c14fb2d8c2363
Scott Merritt <smerrit@purdue.edu>
2018-09-26 22:11:00 -0400

    feature - added fullscreen option in Settings module, still need to fix issue with switch visual persistence when leaving and reloading the Settings page.

--- client/battleship/src/app/settings/settings.component.ts
+++ client/battleship/src/app/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DarkModeService } from './darkmode.service';
 
 @Component({
@@ -7,9 +7,8 @@ import { DarkModeService } from './darkmode.service';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
   darkMode:boolean;
-  
+  fsMode:boolean;
 
   constructor(private dm: DarkModeService) { }
 
@@ -23,6 +22,11 @@ export class SettingsComponent implements OnInit {
     } else {
       body.classList.remove('darkMode');
     }
+
+    if(window.innerHeight != screen.height){
+      const slider: HTMLInputElement = <HTMLInputElement>document.getElementsByClassName('dmtoggle')[0];
+      slider.checked = false;
+    }
   }
 
   toggleDarkMode() {
@@ -32,6 +36,25 @@ export class SettingsComponent implements OnInit {
       body.classList.add('darkMode');
     } else {
       body.classList.remove('darkMode');
+    }
+  }
+
+  @ViewChild('input') private input;
+
+  toggleFullScreen() {
+    if(window.innerHeight != screen.height) { //if not already in fullscreen mode
+      let elem = document.body;
+      let methodToBeInvoked = elem.requestFullscreen ||
+        elem.webkitRequestFullscreen || elem['mozRequestFullscreen'] ||
+        elem['msRequestFullscreen'];
+        if(methodToBeInvoked) methodToBeInvoked.call(elem);
+    }
+    else { //else exit fullscreen
+      this.input.nativeElement.focus();
+      let startPos = this.input.nativeElement.selectionStart;
+      let value = this.input.nativeElement.value;
+      this.input.nativeElement.value=
+      value.substring(0, startPos) + 'YEEEEEET ' + value.substring(startPos, value.length)
     }
   }
 
