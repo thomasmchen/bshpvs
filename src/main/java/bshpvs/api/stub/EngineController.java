c7d4b8bd44ad27a9f96396bef2014ef0722a045e
Dan Morton <dmorton2297@gmail.com>
2018-10-12 11:47:43 -0400

    Fixed new game menu to have appropriately defined ships

--- src/main/java/bshpvs/api/stub/EngineController.java
+++ src/main/java/bshpvs/api/stub/EngineController.java
@@ -1,6 +1,9 @@
 package bshpvs.api.stub;
 
 import bshpvs.api.core.NewGameRequest;
+import bshpvs.model.Game;
+import bshpvs.model.Player;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class EngineController {
 
 
+    public Player playerOne;
+    public Player playerTwo;
+    public Game game;
+
     @CrossOrigin
     @MessageMapping("/placeShips")
     @SendTo("/topic/getUserShips")
@@ -20,9 +27,19 @@ public class EngineController {
         Thread.sleep(1000); // simulated delay
         ObjectMapper objectMapper = new ObjectMapper();
         NewGameRequest req = objectMapper.readValue(json, NewGameRequest.class);
-
+        
         System.out.println(req.toString());
         //return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
         return null;
+    }
+
+
+    public void initializePlayers() {
+        this.playerOne = new Player(10);
+        this.playerTwo = new Player(10);
+    }
+
+    public void initializeGame() {
+        game = new Game(this.playerOne, this.playerTwo, 10);
     }
 }
