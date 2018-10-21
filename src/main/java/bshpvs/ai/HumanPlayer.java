bbfa8bb59297db52649b7d2e4f7bdb2505a2f347
Thomas <thomas99k@gmail.com>
2018-10-21 04:50:53 -0400

    core - refactor ai package to support better idiomatic usage of Playable interface

---
+++ src/main/java/bshpvs/ai/HumanPlayer.java
@@ -0,0 +1,12 @@
+package bshpvs.ai;
+
+import bshpvs.model.Player;
+
+import java.awt.*;
+
+public class HumanPlayer implements Playable {
+    @Override
+    public Point move(Player pl) {
+        return null;
+    }
+}
