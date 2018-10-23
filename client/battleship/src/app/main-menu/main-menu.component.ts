e8dade716748562b5fa6730d000ec90059777f79
kelvinchoi95 <kelvinchoi95@gmail.com>
2018-10-23 06:51:45 -0400

    LogOut functionality and basic stats page layout created

--- client/battleship/src/app/main-menu/main-menu.component.ts
+++ client/battleship/src/app/main-menu/main-menu.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone} from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 import { DarkModeService } from '../settings/darkmode.service';
-
+declare var gapi : any;
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
+
 })
+
 export class MainMenuComponent implements OnInit {
 
   darkMode:boolean;
 
-  constructor(private router: Router, private dm: DarkModeService) { }
+
+  constructor(private router: Router, private dm: DarkModeService, private auth: AuthService) {}
 
   ngOnInit() {
     this.dm.currentDarkMode.subscribe(darkMode => this.darkMode = darkMode);
@@ -45,5 +49,17 @@ export class MainMenuComponent implements OnInit {
   aboutClicked() {
     this.router.navigateByUrl('/about');
   }
+  logOutClicked() {
+    //declare var gapi : any;
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+
+
+    this.router.navigateByUrl('/login');
+    //onSignIn(auth2);
+  }
+
 
 }
