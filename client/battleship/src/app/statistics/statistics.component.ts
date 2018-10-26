daaf281b6b39af3d7cff58b9810d851ca237f33c
Scott Merritt <smerrit@purdue.edu>
2018-10-26 00:15:15 -0400

    removed old testing values from client side

--- client/battleship/src/app/statistics/statistics.component.ts
+++ client/battleship/src/app/statistics/statistics.component.ts
@@ -5,28 +5,6 @@ import { AuthService } from '../auth.service';
 import { WebService } from '../web.service';
 import { Subscription } from 'rxjs';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  /*
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  */
-];
-
 
 @Component({
   selector: 'app-statistics',
@@ -65,14 +43,6 @@ export class StatisticsComponent implements OnInit {
       console.log(r);
       this.stats = r.statsList;
     }); 
-/*
-    //for testing if data was received 
-    var count = 0;
-    while(this.stats[count] != null){
-      alert(this.stats[count].numPlayer);
-      count++;
-    }
-  */
   }
 
 }
