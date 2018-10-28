87a7ba8d1efedd1310b8155e64f09cc5bb5be080
Dan Morton <dmorton2297@gmail.com>
2018-10-27 20:21:35 -0400

    Defect 11 - New game menu allows user to place diagnol ships

--- client/battleship/src/app/new-game-menu/new-game-menu.component.ts
+++ client/battleship/src/app/new-game-menu/new-game-menu.component.ts
@@ -139,6 +139,8 @@ export class NewGameMenuComponent implements OnInit {
     }
     var tempShip = ship;
     let colinearTest = this.checkShipColinear(ship, {x: event.col, y: event.row});
+    let diagnolTest = this.checkShipDiagnol(ship, {x: event.col, y: event.row});
+   
     if (!colinearTest && ship.spaces.length >= 2) {
       return false;
     }
@@ -198,7 +200,7 @@ export class NewGameMenuComponent implements OnInit {
   }
 
   checkShipDiagnol(ship: Ship, coordinate: Coordinate) {
-    if (ship.spaces.length < 2) {
+    if (ship.spaces.length < 1) {
       return true;
     } else {
       var tempSpaces = new Array<Coordinate>();
@@ -208,8 +210,13 @@ export class NewGameMenuComponent implements OnInit {
         tempSpaces.push(tempSpace);
       }
 
-      tempSpaces.push(coordinate);
+      if (coordinate.x != tempSpaces[tempSpaces.length - 1].x && coordinate.y != tempSpaces[tempSpaces.length - 1].y) {
+        return true;
+      }
+
     }
+
+    return false;
   }
 
 
